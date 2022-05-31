ce8502a39ec9a53e25b85218c69a63097649dfaf
Rahul Aggarwal <rahul.aggarwal000005@gmail.com>
2022-05-31 22:58:00 +0530

    Learned about the drawer widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,10 +13,9 @@ class HomePage extends StatelessWidget {
         title: const Text("Catalog App"),
       ),
       body: Center(
-        child: Container(
-          child: Text("$days days of $courseName"),
-        ),
+        child: Text("$days days of $courseName"),
       ),
+      drawer: MyDrawer(),
     );
   }
 }
