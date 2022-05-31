2f1ed86529b3e1c3d2a5f392fa916026bf4e3c9b
Rahul Aggarwal <rahul.aggarwal000005@gmail.com>
2022-05-31 23:31:09 +0530

    Created the model for the catalog and added the dummy data.

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+class Item {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final Object rating;
+
+  Item(this.id, this.title, this.price, this.description, this.category,
+      this.image, this.rating);
+}
