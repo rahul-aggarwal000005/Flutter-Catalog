3b25bc0ba1c3e85f02a66b1ce3da256e1487d561
Rahul Aggarwal <rahul.aggarwal000005@gmail.com>
2022-06-01 11:17:18 +0530

    build the catalog item widget and rendered the dummy item

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -10,3 +10,17 @@ class Item {
   Item(this.id, this.title, this.price, this.description, this.category,
       this.image, this.rating);
 }
+
+class CatalogModel {
+  static final items = [
+    Item(
+      1,
+      "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      10.3,
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      "men's clothing",
+      "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      {"rate": 3.9, "count": 120},
+    )
+  ];
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,14 +11,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(
-        child: Text("$days days of $courseName"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
